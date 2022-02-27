533801c76117d32f6c4b4c0bb0c68fb628913f8b
Alec Tufenkjian <alec.tufenkjian@mail.mcgill.ca>
2022-02-26 21:30:48 -0500

    Fixed bug that was failing the deploy

--- src/components/HighlightedProjectSection.js
+++ src/components/HighlightedProjectSection.js
@@ -202,7 +202,7 @@ export default function HighlightedProjectSection() {
                   <h3 className="project-sub-title">{project.subtitle}</h3>
                   <p className="project-desc">{project.description}</p>
                   <div className="buttons">
-                    <a href="#" className="primary-btn outline external-link"> <span>Try it out!</span>
+                    <a href="" className="primary-btn outline external-link"> <span>Try it out!</span>
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                           d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
