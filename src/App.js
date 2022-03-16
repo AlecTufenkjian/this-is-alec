0a06dd521703fc96a9d10ee82b8f1f8951aa528e
Alec Tufenkjian <alec.tufenkjian@mail.mcgill.ca>
2022-03-16 14:51:49 -0400

    Switched from BrowseRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import NavMenu from './components/NavMenu';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
