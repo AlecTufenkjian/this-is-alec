affe8dafa06afb5d8d178ec2a2bfebbb9f36bbf4
Alec Tufenkjian <alec.tufenkjian@mail.mcgill.ca>
2022-04-27 21:24:46 -0400

    Update on discord function

--- netlify/functions/discord.js
+++ netlify/functions/discord.js
@@ -1,6 +1,37 @@
 exports.handler = async function(event, context) {
+    const displayName = `Anonymous`;
+
+    const content = {
+      "content": "",
+      "embeds": [{
+        "title": `A message from: ${displayName}`,
+        "description": `message`,
+        "footer": {
+          "text": `email`
+        }
+      }]
+    }
+
+    fetch('https://discord.com/api/webhooks/969046291134951484/BcoybJydG0L6kO8eNDTpJvbAU6KNhM0dxX92g0NB1zPtJAWgGAk3iaUeTE7R89wcQITd', {
+      method: 'POST',
+      body: JSON.stringify(content),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "Hello World" }),
+        body: JSON.stringify({ message: "Success" }),
     };
+
+
+
+
 }
+
+
+
+
+
+    
