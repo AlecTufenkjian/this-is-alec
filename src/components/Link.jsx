433c733b7421173ab991d38ee09e2dc2124540c6
Alec Tufenkjian <alectufenkjian@gmail.com>
2024-09-25 18:53:47 -0400

    Minor changes

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -6,6 +6,60 @@ import PropTypes from "prop-types";
 export const Link = ({ link, icon, name, copy, size }) => {
   const [copied, setCopied] = useState(false);
 
+  const pingDiscord = () => {
+    const userAgent = navigator.userAgent;  
+    let browserName, browserVersion, platform, language, isMobile;
+
+    const browserPatterns = [
+      {regex: /Edg\/([\d.]+)/, name: 'Microsoft Edge' },
+      { regex: /Chrome\/([\d.]+)/, name: 'Google Chrome' },
+      { regex: /Firefox\/([\d.]+)/, name: 'Mozilla Firefox' },
+      { regex: /Safari\/([\d.]+)/, name: 'Safari' },
+      { regex: /Opera\/.*Version\/([\d.]+)/, name: 'Opera' },
+      { regex: /(?:MSIE|Trident.*rv:)(?:([\d.]+))?/, name: 'Internet Explorer' },
+    ];
+
+    for(let pattern of browserPatterns) {
+        if(pattern.regex.test(userAgent)) {
+            browserName = pattern.name;
+            browserVersion = userAgent.match(pattern.regex)[1];
+            break;
+        }
+    }
+
+    if(!browserName) {
+        browserName = 'Unknown';
+        browserVersion = 'Unknown';
+    }
+
+    function getIP() {
+      return fetch('https://api.ipify.org?format=json')
+        .then(response => response.json())
+        .then(data => { return data.ip; })
+        .catch(error => { return "Unknown"; });
+    }
+
+    isMobile = /Mobile|mini|Fennec|Android|iP(ad|od|hone)/.test(userAgent);
+
+    getIP().then(ip => fetch('.netlify/functions/contact_me_via_discord', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: "Tracker",
+        message: `
+        Someone accessed your ${name}
+        Browser Name: ${browserName}
+        Browser Version: ${browserVersion}
+        Mobile Browser: ${isMobile ? "Yes" : "No"}
+        IP: ${ip}
+        Screen Size: ${window.screen.width + 'x' + window.screen.height}`,
+        email: "",
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }))
+  }
+
   if (link !== null) {
     return (
       <>
@@ -14,6 +68,7 @@ export const Link = ({ link, icon, name, copy, size }) => {
           href={link}
           target="_blank"
           rel="noopener noreferrer"
+          onClick={pingDiscord}
         >
           <span className="fa">
             <FontAwesomeIcon icon={icon} size={size} />
